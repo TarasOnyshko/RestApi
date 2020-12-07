b32b33a4c314ecfc3752648a05f22868a664b228
Sika <k.sofiya1313@gmail.com>
2020-12-07 17:51:01 +0200

    added ItemDetailView Model

---
+++ ListView_Sample/Model/ItemDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView_Sample.Model
+{
+    class ItemDetailViewModel
+    {
+        public Item Item { get; set; }
+
+        public ItemDetailViewModel(Item item = null)
+        {
+            //title = item?.title;
+            Item = item;
+        }
+    }
+}
